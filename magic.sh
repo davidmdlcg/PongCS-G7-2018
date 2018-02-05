101d7593c979a6996c50f04362fde91c128450f8
Raul Martin <r.martinsanta@alumnos.urjc.es>
2018-02-05 19:23:24 +0100

    Updated Gradle, added Travis integration

---
+++ magic.sh
@@ -0,0 +1,24 @@
+BRANCH="master"
+
+# Only for master
+if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
+  
+  # Is this not a Pull Request?
+  if [ "$TRAVIS_PULL_REQUEST" = false ]; then
+    
+    # Is this not a build which was triggered by setting a new tag?
+    if [ -z "$TRAVIS_TAG" ]; then
+      echo -e "Tagging commit.\n"
+
+      git config --global user.email "travis@travis-ci.org"
+      git config --global user.name "Travis"
+
+      # Add tag and push to master.
+      git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
+      git push origin --tags
+      git fetch origin
+
+      echo -e "Magic done.\n"
+  fi
+  fi
+fi
