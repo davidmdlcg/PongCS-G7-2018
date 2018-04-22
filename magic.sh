0242bc12cf59769045c94998672463aede33ceb3
ExtremoBlando <r.martinsanta@alumnos.urjc.es>
2018-04-22 23:54:41 +0200

    Up

--- magic.sh
+++ magic.sh
@@ -8,13 +8,13 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
     
     # Is this not a build which was triggered by setting a new tag?
     if [ -z "$TRAVIS_TAG" ]; then
-      echo -e "Tagging commit.\n"
+      #echo -e "Tagging commit.\n"
 
       git config --global user.email "travis@travis-ci.org"
       git config --global user.name "Travis"
 
       # Add tag and push to master.
-      git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
+      #git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
       #git fetch origin
       
       # Upload doc file
@@ -22,7 +22,7 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
       git add .
       git commit -m "[skip ci] TRAVIS Updated cucumber report"
       echo "Magic: pushing changes"
-      git push https://ExtremoBlando:$MAGIC_TOKEN@github.com/jjluczyn/PongCS-G7-2018 master --tags    
+      git push https://ExtremoBlando:$MAGIC_TOKEN@github.com/jjluczyn/PongCS-G7-2018 master #--tags    
       echo -e "Magic done.\n"
   fi
   fi
