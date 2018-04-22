9e99976de1ba182ca94c54b74c88302c8306088a
ExtremoBlando <r.martinsanta@alumnos.urjc.es>
2018-04-23 00:03:52 +0200

    Update magic.sh

--- magic.sh
+++ magic.sh
@@ -13,6 +13,7 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
       git config --global user.email "travis@travis-ci.org"
       git config --global user.name "Travis"
 
+      git checkout master
       # Add tag and push to master.
       #git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
       #git fetch origin
